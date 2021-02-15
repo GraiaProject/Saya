ba9bf270ad5e91c51cde7e3d4e67d3253c4f9e7c
GreyElaina <31543961+Gr0eyElaina@users.noreply.github.com>
2021-02-16 01:29:47 +0800

    add example

---
+++ example/first-one/main.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from graia.saya import Saya
+from graia.broadcast import Broadcast
+from graia.saya.builtins.broadcast import BroadcastBehaviour
+
+loop = asyncio.get_event_loop()
+broadcast = Broadcast(loop=loop)
+saya = Saya(broadcast)
+saya.install_behaviours(BroadcastBehaviour(broadcast))
+
+with saya.module_context():
+    saya.require("modules.module_as_file")
+    saya.require("modules.module_as_dir")
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    exit()
